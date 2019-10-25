0b8a3b446cb5723041f163f5c3665c90ef285e75
jitu <jitu@lifesight.io>
2019-10-25 12:55:21 +0530

    Added download button

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -16,11 +16,34 @@ class Editor extends Component {
             this.setState({data: newData.json, value: newData.json})
         })
     }
+
+    download = () => {
+        let filename = "export.json";
+        let contentType = "application/json;charset=utf-8;";
+        if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+            var blob = new Blob([decodeURIComponent(encodeURI(JSON.stringify(this.state.data)))], { type: contentType });            
+            navigator.msSaveOrOpenBlob(blob, filename);
+        } else {
+            var a = document.createElement('a');
+            a.download = filename;
+            a.href = 'data:' + contentType + ',' + encodeURIComponent(JSON.stringify(this.state.data));
+            a.target = '_blank';
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+    }
     render() {        
 
         return (
             <div className='tree'>
-                <pre>{ JSON.stringify( this.state.data, null, 2)}</pre>
+                <div className="json-file">
+                    <pre>
+                        { JSON.stringify( this.state.data, null, 2)}
+                    </pre>
+                    <button onClick={this.download} className='download-btn button-success'>Download</button> 
+                </div>
+                               
                 <RootObject data={this.state.value} />
             </div>
         )
